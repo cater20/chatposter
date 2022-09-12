adccefd4c213e22a44b368a3d7e990e54d362323
cater <103543555+cater20@users.noreply.github.com>
2022-09-12 09:14:37 +0300

    Create seeds.rb

---
+++ backend-ruby/db/seeds.rb
@@ -0,0 +1,26 @@
+puts "🌱 Seeding spices..."
+
+# Seed your database here
+Post.create([
+
+    {author:"john", title: "when the sun goes down",
+    body: " what a scummy man just give him half a chance I bet he'll rob you if he can see it in his eyes, yeah .That he's got a driving ban amongst some other offences"
+},
+
+    {author:"john",title: "River and the source",
+    body: "I wonder what went wrong so that she had to roam the streets she don't do major credit cards I doubt she does receipts It's all not quite legitimate"
+    },
+
+    {author:"john",title: "betrayal in the city",
+    body: "Avert your eyes and staring at the ground She makes a subtle proposition I'm sorry love I'll have to turn you down"
+},
+
+    {author:"john",title: "kidagaa kimemwozea",
+    body: "sema sema ni mwonga kwamba mwishowe hulia kibinji,usisahau ya leo ni leo ila ya kesho ni janga la utilivu mtupu"
+},
+{
+    author:"john",title: "far from home",
+    body: "the city in not a childs place to play its home not home so far from papa and mama,a home not safe for us ,we better go back home"
+}
+])
+puts "✅ Done seeding!"
